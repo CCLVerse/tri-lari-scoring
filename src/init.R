4225d2663337346b9d2d48a5874a1d4610191659
Sirish Shrestha <sirish.shrestha@gmail.com>
2023-04-28 11:03:09 -0400

    updated .gitignore to remove data and datamaps

--- src/init.R
+++ src/init.R
@@ -1,11 +1,15 @@
 source('./src/load_packages.R')
 ## Load all required packages
-load_packages(c('tidyverse', 'log4r', 'config'))
+load_packages(c('tidyverse', 'log4r', 'config','lm.beta'))
 
 ## source all functions from the lari folder
 ws.folder <- "./src/lari/"
 ws.files <- list.files(ws.folder)
-invisible(sapply(paste0(ws.folder,ws.files), source))
+
+# Load all files if exist
+if(length(ws.files > 0)){
+  invisible(sapply(paste0(ws.folder,ws.files), source))
+}
 
 
 ## logging 
