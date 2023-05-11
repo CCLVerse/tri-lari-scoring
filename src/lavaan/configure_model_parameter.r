56fc2cc0eb454ac50e59ba99efe46543934c21a0
Sirish Shrestha <sirish.shrestha@gmail.com>
2023-05-11 14:12:33 -0400

    added model fit function to calculate fit values

---
+++ src/lavaan/configure_model_parameter.r
@@ -0,0 +1,31 @@
+#' @title Configure Model Parameters
+#' @description Configuring parameters needed to build the model by checking and validating, and by conforming the structure before applying to the model. This is a helper function.
+#' @param df Dataframe to work with
+#' @param competency_cols A character vector of required columns for building the model. It is optional if `competency_cols_patter` is provided. 
+#' @param factor_name A name that describes the compentency or the factor for which the model is being created. 
+#' @param raters A character vector of all raters that are required in the model. 
+#' @param regex_pattern Boolean to indicate whether the column_names consists of regular expression pattern or column names vector. Default is FALSE.
+#' @return Returns the character of the model
+configure_model_parameters <- function(df=data.frame(), raters=NULL, factor_name=NULL, competency_cols=NULL,regex_pattern=FALSE){
+
+    column_names <- as.character()
+
+    if(regex_pattern){
+        column_names <- get_column_names(df=df, pattern=competency_cols)
+
+    } else {
+        column_names <- competency_cols
+        validate_cols(df, column_names)
+    }
+
+    raters <- raters %>% str_replace_all(., "\\s+", "_")
+    
+    model_params <- list(
+        raters = raters, 
+        factor_name = factor_name, 
+        column_names = column_names
+    )
+
+    return(model_params)
+
+}
---
+++ src/lavaan/lavaan_model_fit.r
@@ -0,0 +1,45 @@
+#' @title Fit the model
+#' @description Fit the model based on the type and the model provided.  
+#' @param df Dataframe to work with
+#' @param model A character model with latent variables that lavaan requires. See `https://www.lavaan.ugent.be` for more details. 
+lavaan_model_fit <- function(df=data.frame(), model=as.character()){
+
+    validate_df(df)
+
+    if(!is.character(model)){
+        stop("Model doesn't seem to be proper format.")
+    }
+
+
+    ## Oblique fit
+    oblique.fit <- lavaan::cfa(model, data=df)
+    oblique.summary <- get_fit_summary(fit=oblique.fit)
+    oblique.fitted <- lavaan::lavPredict(oblique.fit, fsm=TRUE, method="regression")
+
+    oblique <- list(fit=oblique.fit, 
+                    summary=oblique.summary,
+                    fitted=oblique.fitted)
+
+    ## orthogonal fit
+    ortho.fit <- lavaan::cfa(model, data=df, orthogonal=TRUE)
+    ortho.summary <- get_fit_summary(fit=ortho.fit)
+    ortho.fitted <- lavaan::lavPredict(ortho.fit, fsm=TRUE, method="regression")
+
+    orthogonal <- list(fit=ortho.fit, 
+                       summary=ortho.summary, 
+                       fitted=ortho.fitted)
+
+    return(list(oblique=oblique, orthogonal=orthogonal))
+}
+
+
+
+get_fit_summary <- function(fit=NULL){
+    
+    if(is.null(fit) && is.na(fit)){
+        stop("Model fit is required.")
+    }
+    s <- lavaan::summary(fit, fit.measures = TRUE)
+
+    return(s)
+}
